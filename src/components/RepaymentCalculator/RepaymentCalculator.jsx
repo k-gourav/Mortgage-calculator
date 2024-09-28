21f94e898cdf0b8add17ab6c70bee4c23b4239de
k-gourav <code2change.gourav@gmail.com>
2024-09-28 23:55:17 +0530

    feat: add initial Mortgage Calculator component

--- src/components/RepaymentCalculator/RepaymentCalculator.jsx
+++ src/components/RepaymentCalculator/RepaymentCalculator.jsx
@@ -1,7 +1,18 @@
+import styles from "./RepaymentCalculator.module.css";
 const RepaymentCalculator = () => {
-    return (
-        <div>Hello</div>
-    )
-}
+  return (
+    <main className={styles.main__element}>
+      <div className={styles.repayment__container}>
+        <div className={styles.repayment__calculator}>
+            
+
+        </div>
+        <div className={styles.repayment__banner}>
+
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default RepaymentCalculator;
