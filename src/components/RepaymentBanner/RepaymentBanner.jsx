cdcef70049d3e465a3034419a32e8a25645d9808
k-gourav <code2change.gourav@gmail.com>
2024-10-03 18:32:41 +0530

    feat: add styling to the complete layout

--- src/components/RepaymentBanner/RepaymentBanner.jsx
+++ src/components/RepaymentBanner/RepaymentBanner.jsx
@@ -5,9 +5,11 @@ import styles from './RepaymentBanner.module.css'
 function RepaymentBanner() {
   return (
     <div className={styles.repayment__banner}>
-      <img src={calculatorImage} alt="Calculator Image" />
-      
-        
+      <img src={calculatorImage} alt="Calculator Image" width="180" />
+      <div className={styles.banner__content}>
+      <h2 className={styles.banner__title}>Results shown here</h2>
+      <p id={styles.banner__body}>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+      </div>
     </div>
   )
 }
